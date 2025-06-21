1fd2d7c27f4bbf4b5121b5bd27f6eead9d9491a2
Andrei Romanov <ansromanov@gmail.com>
2025-06-21 15:41:27 +0200

    add python structure

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+""" Top level package for the ci-cli app """
+
+__app_name__ = "ci-cli"
+__version__ = "0.0.1"
---
+++ app/cli.py
@@ -0,0 +1,25 @@
+import typer
+
+from app import __app_name__, __version__
+
+app = typer.Typer()
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+    
+@app.callback()
+
+def main(
+    version: bool = typer.Option(
+        None,
+        "--version",
+        "-v",
+        callback=_version_callback,
+        is_eager=True,
+        help="Show application version.",
+    ),
+) -> None:
+    """A CLI tool for managing CI/CD pipelines."""
+    pass
